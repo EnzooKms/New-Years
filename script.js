1e6d604481da8fa611e9dcbca93164c2355814a2
EnzooKms <ekemmas@edenschool.fr>
2022-09-12 19:28:12 +0200

    add all

---
+++ script.js
@@ -0,0 +1,37 @@
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minsEl = document.getElementById('mins')
+const secondsEl = document.getElementById('seconds')
+const titre = document.getElementById("titre")
+
+let newYears = '9 jun 2022 12:00:00'
+
+titre.innerHTML = `Réveillon de Nouvel an : ${newYears}`
+
+function countdown() {
+    const newYearsDate = new Date(newYears)
+    const currentDate = new Date()
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000
+
+    const days = Math.floor(totalSeconds / 3600 / 24)
+
+    const hours = Math.floor(totalSeconds / 3600) % 24
+
+    const minutes = Math.floor(totalSeconds / 60) % 60
+
+    const seconds = Math.floor(totalSeconds) % 60
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time
+}
+
+countdown()
+
+setInterval(countdown, 1000)
